b8da96b78c5c4c6c2a3ca85f37ba4aff8a5a6dfa
Beth <beth.swingler@gmail.com>
2018-11-12 22:44:02 +0000

    created game logic but better

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,17 +3,21 @@ console.log("Up and running!");
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
-var cardOne = cards [0];
-cardsInPlay.push ('cardOne');
-console.log('User flipped ' + cardOne);
-
-var cardTwo = cards [2];
-cardsInPlay.push ('cardTwo');
-console.log('User flipped ' + cardTwo);
-
-if ((cardsInPlay.length === 2) && (cardsInPlay [0] === cardsInPlay [1])) {
+var checkForMatch = function () {
+if ((cardsInPlay.length >= 2) && (cardsInPlay [0] === cardsInPlay [1])) {
 		alert("You found a match, SNAP!");
 		} else {
 			alert("Sorry, try again.")
 		}
-	
+}
+
+var flipCard = function(cardId) {
+console.log("You gone done flipped " + cards[cardId]);
+cardsInPlay.push (cards[cardId]);
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
